9745496f7f1dcbddf61543be64a106ee9f1ec2c8
Keval Shah <kshah2451@gmail.com>
2014-03-12 00:19:42 -0700

    Added functionality for take picture button. Can now access camera, take picture and save to SketchPlay directory in Pictures directory on SD card.

--- SketchPlay/src/com/orlly/sketchplay/StartGameMenu.java
+++ SketchPlay/src/com/orlly/sketchplay/StartGameMenu.java
@@ -1,14 +1,38 @@
 package com.orlly.sketchplay;
 
+import java.io.File;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 public class StartGameMenu extends Activity {
 	
+	/**
+	 * Button views
+	 */
 	Button take_picture;
 	Button import_picture;
+	
+	private static final int CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE = 100;
+	
+	public static final int MEDIA_TYPE_IMAGE = 1;
+	
+	/**
+	 * Uniform Resource Identifier - an address that identifies an abstract
+	 * or physical resource.
+	 */
+	private Uri fileUri; 
+	
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -18,8 +42,111 @@ public class StartGameMenu extends Activity {
 		import_picture = (Button)findViewById(R.id.import_picture_button);
 	}
 	
+	
+	/**
+	 * Function called when "take a picture" button is pressed
+	 * @param view
+	 */
 	public void takePicture(View view) {
+		// Create new intent to have camera application capture an image and return it
+		Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
 		
+		// Creates and returns a file Uri for saving an image
+		fileUri = getOutputMediaFileUri(MEDIA_TYPE_IMAGE);
+		
+		// Add extended data to the intent
+		intent.putExtra(MediaStore.EXTRA_OUTPUT, fileUri);
+		
+		// Start the intent
+		startActivityForResult(intent, CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE);
+	}
+	
+	/**
+	 * Receives the output/result of camera intent
+	 */
+	@Override
+	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+		super.onActivityResult(requestCode, resultCode, data);
+		if(requestCode == CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE) {
+			if(resultCode == RESULT_OK) {
+			// If image captured and saved to fileUri specified in Intent then...
+				
+				Toast.makeText(this, "Image saved", Toast.LENGTH_LONG).show();
+				
+				// TODO: Add code to do image manipulation / conversion
+			
+			}
+			else if(resultCode == RESULT_CANCELED) {
+			// If user cancelled the image capture then...
+				
+				Toast.makeText(this, "Cancelled image capture", Toast.LENGTH_LONG).show();
+				
+				// TODO: Add code to do something		
+			}
+			else {
+			// If image capture failed then...
+				
+				Toast.makeText(this, "Image capture failed", Toast.LENGTH_LONG).show();
+				
+				// TODO: Add code to do something
+			}
+		}
+	}
+
+
+	/**
+	 * Function called when "import picture" button is pressed
+	 * @param view
+	 */
+	public void importPicture(View view) {
+		
+	}
+	
+	
+	/**
+	 * Create a file Uri for saving an image
+	 * @param type
+	 * @return
+	 */
+	private static Uri getOutputMediaFileUri(int type){
+	      return Uri.fromFile(getOutputMediaFile(type));
+	}
+	
+	
+	/**
+	 * Create a File for saving an image
+	 * @param type
+	 * @return
+	 */
+	private static File getOutputMediaFile(int type){
+	    // To be safe, you should check that the SDCard is mounted
+	    // using Environment.getExternalStorageState() before doing this.
+		
+		// Constructs a new file using the specified directory and name
+	    File mediaStorageDir = new File(Environment.getExternalStoragePublicDirectory(
+	              Environment.DIRECTORY_PICTURES), "SketchPlay");
+
+	    // Create the storage directory if it does not exist
+	    if (! mediaStorageDir.exists()){
+	        if (! mediaStorageDir.mkdirs()){
+	            Log.d("SketchPlay", "failed to create directory");
+	            return null;
+	        }
+	    }
+
+	    /* Create a media file name */
+	    // Create a timestamp with current time in specified format
+	    String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+	    File mediaFile;
+	    if (type == MEDIA_TYPE_IMAGE){
+	    	// Construct a new file with the specified path
+	        mediaFile = new File(mediaStorageDir.getPath() + File.separator +
+	        "IMG_"+ timeStamp + ".jpg");
+	    } else {
+	        return null;
+	    }
+
+	    return mediaFile;
 	}
 	
 }
