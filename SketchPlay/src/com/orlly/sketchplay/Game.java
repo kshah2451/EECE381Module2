e94197f1df96ab2e0d42bf85481d1aecc08d06ad
Keval Shah <kshah2451@gmail.com>
2014-03-13 16:16:07 -0700

    Created activity for main game. Edited manifest to reflect this. Added temporary functionality to play button in ImageSelectorMenu. Created temporary options.xml layout.

--- SketchPlay/src/com/orlly/sketchplay/Game.java
+++ SketchPlay/src/com/orlly/sketchplay/Game.java
@@ -1,8 +1,15 @@
 package com.orlly.sketchplay;
 
 import android.app.Activity;
+import android.os.Bundle;
 
 public class Game extends Activity{
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.game);
+	}
 
 	
 	
