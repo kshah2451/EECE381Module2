603860971185191ef8747100f42791e1685f7d37
Keval Shah <kshah2451@gmail.com>
2014-03-19 14:42:47 -0700

    Added bundle so that parameters get passed to Game activity when Play Game button is pressed. Added comments.

--- SketchPlay/src/com/orlly/sketchplay/ImageAdjustmentMenu.java
+++ SketchPlay/src/com/orlly/sketchplay/ImageAdjustmentMenu.java
@@ -1,95 +1,98 @@
 package com.orlly.sketchplay;
 
-import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.IOException;
-import java.io.InputStream;
-import java.text.SimpleDateFormat;
-import java.util.Date;
 
 import android.annotation.TargetApi;
 import android.app.Activity;
 import android.content.Intent;
 import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
 import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
-import android.os.Environment;
 import android.provider.MediaStore;
-import android.util.Log;
-import android.view.Menu;
-import android.view.MenuInflater;
-import android.view.MenuItem;
 import android.view.View;
-import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.SeekBar;
-import android.widget.SeekBar.OnSeekBarChangeListener;
-import android.widget.Toast;
 
-public class ImageAdjustmentMenu extends Activity{
-	
-	private Button play_game;
-	private Button render_image;
+public class ImageAdjustmentMenu extends Activity {
+
 	private SeekBar saturation;
 	private SeekBar value;
-	private int saturation_tracker=50;
-	private int value_tracker=50;
+	private int saturation_tracker = 50;
+	private int value_tracker = 50;
 	private ImageView rendering_preview;
 	private Uri imageUri;
 	private Bitmap bitmap;
 	private MapRender rendering;
-	
+
 	@TargetApi(Build.VERSION_CODES.HONEYCOMB)
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.adjustment_menu);
 		getActionBar().setDisplayHomeAsUpEnabled(false);
-		
+
 		Intent intent = new Intent();
 		intent = getIntent();
 		imageUri = Uri.parse(intent.getStringExtra("imageUri"));
-		
+
 		// Find views by id attributes identified in XML file
-		play_game = (Button)findViewById(R.id.play_game_button);
-		render_image = (Button)findViewById(R.id.render_picture_button);
-		saturation = (SeekBar)findViewById(R.id.saturation);
-		value = (SeekBar)findViewById(R.id.value);
-		rendering_preview = (ImageView)findViewById(R.id.img_preview);
-		
+		saturation = (SeekBar) findViewById(R.id.saturation);
+		value = (SeekBar) findViewById(R.id.value);
+		rendering_preview = (ImageView) findViewById(R.id.img_preview);
+
 		try {
-			bitmap = MediaStore.Images.Media.getBitmap(this.getContentResolver(), imageUri);
+			bitmap = MediaStore.Images.Media.getBitmap(
+					this.getContentResolver(), imageUri);
 		} catch (FileNotFoundException e) {
 			e.printStackTrace();
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
 		rendering = new MapRender(bitmap, bitmap.getHeight(), bitmap.getWidth());
-		rendering_preview.setImageBitmap(rendering.getMapImage(50,50));
+		rendering_preview.setImageBitmap(rendering.getMapImage(50, 50));
 	}
-	
-	@Override 
-	protected void onActivityResult(int requestCode, int resultCode, Intent data){
+
+	@Override
+	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
 		super.onActivityResult(requestCode, resultCode, data);
-		rendering_preview.setImageBitmap(rendering.getMapImage(saturation_tracker,value_tracker));
+		rendering_preview.setImageBitmap(rendering.getMapImage(
+				saturation_tracker, value_tracker));
 	}
-	
+
 	/**
 	 * Function called when "Play Game" button is pressed. Launches Game.
+	 * 
 	 * @param view
 	 */
-	public void playGame(View view){		
+	public void playGame(View view) {
 		Intent intent = new Intent(this, Game.class);
-		intent.putExtra("imageUri", imageUri.toString());
+
+		// Create a bundle to put imageURI, saturation, and value to pass to
+		// Game activity
+		Bundle bundle = new Bundle();
+		bundle.putString("imageUri", imageUri.toString());
+		bundle.putInt("saturation", saturation_tracker);
+		bundle.putInt("value", value_tracker);
+		intent.putExtras(bundle);
 		startActivity(intent);
 	}
-	
-	public void renderPicture(View view){
+
+	/**
+	 * Function called when "Render Picture" button is pressed. Renders image in
+	 * preview.
+	 * 
+	 * @param view
+	 */
+	public void renderPicture(View view) {
 		saturation_tracker = saturation.getProgress();
 		value_tracker = value.getProgress();
+		
 		rendering = new MapRender(bitmap, bitmap.getHeight(), bitmap.getWidth());
-		rendering_preview.setImageBitmap(rendering.getMapImage(saturation_tracker,value_tracker));
+		
+		// Set preview to reflect rendered bitmap
+		rendering_preview.setImageBitmap(rendering.getMapImage(
+				saturation_tracker, value_tracker));
 	}
 }
--- SketchPlay/src/com/orlly/sketchplay/MainMenuActivity.java
+++ SketchPlay/src/com/orlly/sketchplay/MainMenuActivity.java
@@ -9,37 +9,17 @@ import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
-import android.widget.Button;
 
 public class MainMenuActivity extends Activity {
-
-	/**
-	 * Start Game button
-	 */
-	Button start_game;
-
-	/**
-	 * Options button
-	 */
-	Button options;
-	
-	/**
-	 * How To Play button
-	 */
-	Button getting_started;
 
 	@TargetApi(Build.VERSION_CODES.HONEYCOMB)
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		start_game = (Button) findViewById(R.id.start_button);
-		options = (Button) findViewById(R.id.options_button);
-		getting_started = (Button) findViewById(R.id.getting_started_button);
 		setContentView(R.layout.main_menu);
 		getActionBar().setDisplayHomeAsUpEnabled(false);
 	}
-	
-	
+		
 
 	/**
 	 * Function called when "start game" button is pressed. Launches
@@ -52,6 +32,7 @@ public class MainMenuActivity extends Activity {
 		startActivity(intent);
 	}
 
+	
 	/**
 	 * Function called when "options" button is pressed. Launches OptionsMenu
 	 * activity.
@@ -63,6 +44,7 @@ public class MainMenuActivity extends Activity {
 		startActivity(intent);
 	}
 
+	
 	/**
 	 * Function called when "Getting Started" button is pressed. Launches
 	 * GettingStarted activity.
@@ -73,6 +55,7 @@ public class MainMenuActivity extends Activity {
 		Intent intent = new Intent(this, GettingStarted.class);
 		startActivity(intent);
 	}
+	
 	
 	/**
 	 * Function called when "Getting Started" action bar item is pressed. Launches
@@ -86,13 +69,11 @@ public class MainMenuActivity extends Activity {
 		return true;
 	}
 
+	
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		MenuInflater inflater = getMenuInflater();
 		inflater.inflate(R.menu.main_menu, menu); 
 	    return super.onCreateOptionsMenu(menu);
 	}
-	
-	
-
 }
