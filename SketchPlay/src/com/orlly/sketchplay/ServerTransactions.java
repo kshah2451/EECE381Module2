8acd6e29b5b3335f3b7ea5c336974a111d5e7a40
g14-bt <bryce.towns@gmail.com>
2014-04-03 15:54:20 -0700

    added skeleton for java server trans

---
+++ SketchPlay/src/com/orlly/sketchplay/ServerTransactions.java
@@ -0,0 +1,33 @@
+package com.orlly.sketchplay;
+
+import android.graphics.Bitmap;
+
+public class ServerTransactions {
+
+	
+	public static void connectServer(){
+		
+	}
+	
+	public static void closeServer(){
+		
+	}
+	
+	public static void sendServer(){
+		
+	}
+
+	public static Bitmap receiveServer(){
+		
+	}
+
+	
+	private Bitmap arrayToBMP(){
+		
+	}
+
+	private char[] BMPToArray(){
+		
+	}
+
+}
